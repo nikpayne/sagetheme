da16c481cc5e3dd65a93c5f69d06bc8da2762b7e
Billy Everyteen <nikolasapayne@gmail.com>
2016-02-09 11:19:33 -0600

    finished the nav, working on social section and endorsements

--- js/app.js
+++ js/app.js
@@ -1,39 +1,6 @@
 $(document).ready(function() {
-  var isActive = false,
-      origHeight = 70;
 
-  $('.header__hamburger').on('click', function() {
-    isActive = !isActive;
-    var winHeight = $(window).height();
-    $('.header__hamburger').toggleClass("active")
-
-    if(!isActive){
-
-      $('.header__hamburger').animate({
-        height: '70',
-        width: "70"
-      }, 100, function(){ });
-
-      $('.header__ul').animate({
-        width: '70',
-        height: '70'
-      }, 100, function(){ });
-
-
-    } else {
-
-      $('.header__hamburger').animate({
-        height: winHeight,
-        width: "100%"
-      }, 100, function(){ });
-
-      $('.header__ul').animate({
-        height: winHeight,
-        width: "100%",
-        opacity: 1
-      }, 100, function(){ });
-
-    }
-
+  $('.header__nav').on('click', function() {
+    $(this).toggleClass("active");
   });
 });
