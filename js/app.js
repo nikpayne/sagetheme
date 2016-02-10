4eea0fd7d8fca97fd8934222d3cb2e7e07e977e0
Billy Everyteen <nikolasapayne@gmail.com>
2016-02-10 11:21:33 -0600

    finalizing the style

--- js/app.js
+++ js/app.js
@@ -3,7 +3,5 @@ $(document).ready(function() {
   $('.header__nav').on('click', function() {
     $(this).toggleClass("active");
   });
-  $('.header__ul a').on('click', function() {
-    $(this).preventDefault();
-  });
+
 });
